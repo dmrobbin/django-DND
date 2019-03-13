0c06788f938398d6a100dd2f63ae2e2ce06918ef
daryl.robbin <robbin.daryl@gmail.com>
2019-03-13 10:33:48 -0700

    added view and template for character views

--- dnd/models.py
+++ dnd/models.py
@@ -7,19 +7,19 @@ class Post(models.Model):
     
     Name = models.CharField(max_length=200)
     Race = models.CharField(max_length=200, default ='Human')
-    level = models.IntegerField(default =1)
-    Bard = 'Bd'
-    Barbarian = 'Bb'
-    Cleric = 'Cl'
-    Druid = 'Dr'
-    Fighter = 'Fg'
-    Monk = 'Mk'
-    Paladin = 'Pd'
-    Rogue = 'Rg'
-    Ranger = 'Rn'
-    Sorcerer = 'Sr'
-    Wizard = 'Wz'
-    Warlock = 'Wk'
+    Level = models.IntegerField(default =1)
+    Bard = 'Bard'
+    Barbarian = 'Barbarian'
+    Cleric = 'Cleric'
+    Druid = 'Druid'
+    Fighter = 'Figher'
+    Monk = 'Monk'
+    Paladin = 'Paladin'
+    Rogue = 'Rogue'
+    Ranger = 'Ranger'
+    Sorcerer = 'Sorcerer'
+    Wizard = 'Wizard'
+    Warlock = 'Warlock'
     Class = (
         (Bard, 'Bard'),
         (Barbarian, 'Barbarian'),
@@ -34,7 +34,7 @@ class Post(models.Model):
         (Warlock, 'Warlock'),
         (Wizard, 'Wizard'),
     )
-    Class = models.CharField(max_length=2,
+    Class = models.CharField(max_length=12,
         choices = Class,
         default = Bard)
     published_date = models.DateTimeField(blank=True, null=True)
@@ -44,4 +44,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.Name
